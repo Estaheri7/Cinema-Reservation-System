3660d01615ac7d6bb92f7f03e3a82cf041c7493c
erfansalman <estaheri101@gmail.com>
2024-01-09 23:06:26 +0330

    completed some methods

--- account_manager.py
+++ account_manager.py
@@ -1,12 +1,17 @@
 class AccountManager:
-    def __init__(self, email):
-        pass
+    def __init__(self):
+        self.log_accounts = []
 
-    def register(self):
-        pass
+    def register(self, email):
+        if email not in self.log_accounts:
+            self.log_accounts.append(email)
+            return True, "Done!"
+        return False, "Nope!"
 
-    def loggin(self):
-        pass
-
-    def is_valid(self):
-        pass
+    def logout(self, email):
+        if email in self.log_accounts:
+            self.log_accounts.remove(email)
+            return True, "Successfuly logged out!"
+        else:
+            return False, "You are not logged in!"
+        
