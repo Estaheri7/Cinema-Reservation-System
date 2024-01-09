13f266bfc5c9489fbff2289f58b6ca2a3c4d2ad7
erfansalman <estaheri101@gmail.com>
2024-01-09 23:11:01 +0330

    writed person as Abstract class

--- person.py
+++ person.py
@@ -2,11 +2,16 @@ from abc import ABC, abstractmethod
 
 class Person(ABC):
     def __init__(self, name, email, phone, id_):
-        pass
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.id_ = id_
+        self.is_regestered = False
+        self.is_loggin = False
 
     def __str__(self):
-        pass
+        return f"Name: {self.name}"
 
     @abstractmethod
-    def check_role(self):
+    def check_role():
         pass
