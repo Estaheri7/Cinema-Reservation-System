98c2c1c6bc3b9879636d8a426ec408fec0636734
erfansalman <estaheri101@gmail.com>
2024-01-09 23:10:27 +0330

    completed movie and movie fetcher to fetch movies from API

--- movie_manager.py
+++ movie_manager.py
@@ -2,13 +2,36 @@ import requests
 
 class Movie:
     def __init__(self, id_, title, runtime, rating, year, description):
-        pass
+        self.id_ = id_
+        self.title = title
+        self.runtime = runtime
+        self.rating = rating
+        self.year = year
+        self.description = description
+        self.total_reserves = 0
+        self.feedbacks = {}
 
     def __str__(self):
-        pass
+        return f"""\nTitle: {self.title}
+RunTime: {self.runtime}, Rating: {self.rating}, Year: {self.year}
+    Description: {self.description}
+    Total reserves: {self.total_reserves}
+Feedbacks: {self.feedbacks}\n"""
 
 class MovieFetch:
+    API_KEY = "3acfbd8a"
+    BASE_URL = "http://www.omdbapi.com/"
 
     @staticmethod
     def movie_fetch(title):
-        pass
+        params = {"apikey": MovieFetch.API_KEY, "t": title}
+        response = requests.get(MovieFetch.BASE_URL, params=params)
+        data = response.json()
+
+        if data['Response'] == "True":
+            movie = Movie(data['imdbID'], data['Title'], data['Runtime'], data['imdbRating'], data['Year'],
+                        data['Plot'])
+            return movie
+        else:
+            print("Nothing found!")
+            return None
