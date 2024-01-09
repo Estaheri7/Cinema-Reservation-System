60302f195387b238d233dfe5fdf392fbb5f1718a
erfansalman <estaheri101@gmail.com>
2024-01-09 23:07:01 +0330

    completed and addded some methods

--- cinemahall.py
+++ cinemahall.py
@@ -1,12 +1,69 @@
+from account_manager import *
+
 class CinemaHall:
+    account_manager = AccountManager()
+
     def __init__(self, name, total_seats):
-        pass
+        self.name = name
+        self.total_seats = total_seats
+        self.all_reservations = {}
+        self.members = {
+            "user": {},
+            "employee": {}
+        }
+        self.movies = {}
 
     def show_movies(self):
-        pass
+        for movie in self.movies.values():
+            print(movie)
 
     def add_person(self, person):
-        pass
+        if person.email in self.members['user'].values() or person.email in self.members['employee'].values():
+            print(f"Error: Email {person.email} is already registered.")
+            return False
+
+        person_type = type(person).__name__.lower()
+        can_regester, msg = CinemaHall.account_manager.register(person.email)
+
+        if can_regester:
+            if person_type == "user":
+                self.members['user'][person.id_] = person.email
+                print(msg)
+                return True
+            elif person_type == "employee":
+                self.members['employee'][person.id_] = person.email
+                print(msg)
+                return True 
+        else:
+            print(msg)
+            return False
+
+    def log_person(self, person):
+        if person.email in self.members['user'].values() or person.email in self.members['employee'].values():
+            registered, msg = CinemaHall.account_manager.register(person.email)
+            if registered:
+                return True
+            else:
+                print(msg)
+                return False
+        raise ValueError("Invalid email! You should register first!")
+        
+
+
+    
+    def logout_person(self, person):
+        if person.email in self.members['user'].values() or person.email in self.members['employee'].values():
+            logout, msg = CinemaHall.account_manager.logout(person.email)
+
+            if logout:
+                print(msg)
+                return False
+            else:
+                print(msg)
+                return True
+        print("You are not registered!")
+        return True
+
 
     def __str__(self):
-        pass
+        return f"{self.movies}, {self.members}"
