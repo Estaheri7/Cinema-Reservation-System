03d5b2b4b63dceaf48dc8d2006a773302fb91c87
erfansalman <estaheri101@gmail.com>
2024-01-09 23:08:27 +0330

    added attributes

--- feedback.py
+++ feedback.py
@@ -1,6 +1,9 @@
 class Feedback:
     def __init__(self, user_id, movie_id, rating, comment):
-        pass
+        self.user_id = user_id
+        self.movie_id = movie_id
+        self.rating = rating
+        self.comment = comment
 
     def __str__(self):
-        pass
+        return f"{self.comment}"
