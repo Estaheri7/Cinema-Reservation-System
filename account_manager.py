e9b8c638568948678a46cc0583ba738c825b1b94
erfansalman <estaheri101@gmail.com>
2024-01-11 02:52:20 +0330

    added is_valid method

--- account_manager.py
+++ account_manager.py
@@ -1,17 +1,24 @@
+import re
+
 class AccountManager:
     def __init__(self):
         self.log_accounts = []
 
     def register(self, email):
-        if email not in self.log_accounts:
+        if self.is_valid(email) and email not in self.log_accounts:
             self.log_accounts.append(email)
             return True, "Done!"
-        return False, "Nope!"
+        return False, "Invalid email format"
 
     def logout(self, email):
         if email in self.log_accounts:
             self.log_accounts.remove(email)
-            return True, "Successfuly logged out!"
+            return True, "Successfully logged out!"
         else:
             return False, "You are not logged in!"
-        
+
+    @staticmethod
+    def is_valid(email):
+        pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        return re.match(pattern, email)
+
