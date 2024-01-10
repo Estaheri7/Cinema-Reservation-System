ff71ec84b81ac871178018a926086253179a94bc
erfansalman <estaheri101@gmail.com>
2024-01-11 02:54:17 +0330

    raised errors and added remove_movie method

--- employee.py
+++ employee.py
@@ -1,4 +1,5 @@
 from person import *
+from movie_manager import *
 
 class Employee(Person):
     unq_id = 1001
@@ -16,15 +17,42 @@ class Employee(Person):
         return id_
 
     def add_movie(self, movie, cinema_hall):
+
+        if not isinstance(movie, Movie):
+            raise TypeError("Invalid movie! Expected a movie but got none!\n")
+
         if movie.id_ not in cinema_hall.movies:
             cinema_hall.movies[movie.id_] = movie
         else:
             print(f"{movie.title} already exists in {cinema_hall.name} movies!")
 
-    @staticmethod
-    def check_role():
-        print("""Choose between 1-n:
-1- Add a movie to Hall
-2- Display movies
-3- Display reservations
-4- logout""")
+    def remove_movie(self, movie, cinema_hall):
+        if not isinstance(movie, Movie):
+            raise TypeError("Invalid movie! Expected a movie but got none!\n")
+            
+        if movie.id_ in cinema_hall.movies:
+            del cinema_hall.movies[movie.id_]
+        else:
+            print("Movie does not exist")
+    
+    def check_role(self):
+        if self.position == "movie manager":
+            print("""Choose between 1-n:
+    1- Add a movie to Hall
+    2- Remove a move from Hall
+    3- Display movies
+    4- logout""")
+            
+        elif self.position == "user manager":
+            print("""Choose between 1-n:
+    1- Add a user email to Hall
+    2- Remove a user email from Hall
+    3- Display users
+    4- Logout""")
+            
+        elif self.position == "reserve manager":
+            print("""Choose between 1-n:
+    1- Create a reservation for a member
+    2- Cancel a reservation for a member
+    3- Display all reservations
+    4- Logout""")
