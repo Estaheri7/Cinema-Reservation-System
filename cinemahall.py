b050dc244fd2e275bc4c0ca63cd741d727ef967a
erfansalman <estaheri101@gmail.com>
2024-01-11 17:57:37 +0330

    better prints and raised EmailRegisteredAlready

--- cinemahall.py
+++ cinemahall.py
@@ -15,24 +15,29 @@ class CinemaHall:
         self.movies = {}
 
     def show_movies(self):
+        if not self.movies:
+            print("\nEmpty\n")
+            return
         for movie in self.movies.values():
             print(movie)
 
     def show_users(self):
         for m, val in self.members.items():
-            print(f"{m[0].upper()}{m[1:]}s:")
+            print(f"\n{m[0].upper()}{m[1:]}s:")
             for email in val.values():
                 print("<< " + email + " >>")
 
     def show_reserves(self):
+        if not self.all_reservations:
+            print("\nEmpty\n")
+            return
         for e, r in self.all_reservations.items():
             print(f"{e} has reserved {r}")
 
     def add_person(self, person):
         if person.email in self.members['user'].values() or person.email in self.members['employee'].values():
-            print(f"Error: Email {person.email} is already registered.")
-            return False
-
+            raise EmailRegisteredAlready(f"\nError: Email {person.email} is already registered.\n")
+    
         person_type = type(person).__name__.lower()
         can_regester, msg = CinemaHall.account_manager.register(person.email)
 
@@ -57,7 +62,7 @@ class CinemaHall:
             else:
                 print(msg)
                 return False
-        raise EmailNotRegistered("email does not exist! You should register first!")
+        raise EmailNotRegistered("\nEmail does not exist! You should register first!\n")
         
     def logout_person(self, person):
         if person.email in self.members['user'].values() or person.email in self.members['employee'].values():
@@ -69,7 +74,7 @@ class CinemaHall:
             else:
                 print(msg)
                 return True
-        print("You are not registered!")
+        print("\nYou are not registered!\n")
         return True
 
     def remove_user(self, person):
@@ -78,8 +83,8 @@ class CinemaHall:
                 if person.email == e:
                     self.logout_person(person)
                     del v[id_]
-                    return
-        print("User not found!")
+                    return f"\nUser {person.email} removed from {self.name} successfuly!\n"
+        return "\nUser not found!\n"
 
     def __str__(self):
         return f"{self.movies}, {self.members}"
